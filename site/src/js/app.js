e6ba9e562a2dd1ff20f991cf59ffcea52f4639a6
Alex Law <pokemon999101@hotmail.co.uk>
2020-09-11 01:16:53 +0100

    ✨ Add prev and next month buttons

--- site/src/js/app.js
+++ site/src/js/app.js
@@ -1,1 +1,33 @@
 import "alpinejs";
+
+const filterFoods = (foods, shortMonthName) => {
+  shortMonthName = shortMonthName.toLowerCase();
+  return foods.filter(food => {
+    return food.best[shortMonthName];
+  });
+}
+
+const shortMonthName = (monthNum = 0) => {
+  return new Date(1, monthNum, 1).toLocaleString("default", {
+    month: "short"
+  })
+}
+
+window.initApp = function(hugoFoods) {
+  return {
+    foods: filterFoods(
+      hugoFoods,
+      shortMonthName()
+    ),
+    all: hugoFoods,
+    month: new Date().getMonth(),
+    prevMonth() {
+      this.month--;
+      this.foods = filterFoods(hugoFoods, shortMonthName(this.month));
+    },
+    nextMonth() {
+      this.month++;
+      this.foods = filterFoods(hugoFoods, shortMonthName(this.month));
+    }
+  };
+}
