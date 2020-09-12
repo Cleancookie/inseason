32a624c985b7abf64b1ffce154a5f576ec437030
Cleancookie <pokemon999101@hotmail.co.uk>
2020-09-12 17:22:23 +0100

    🐛 Fix bug where previous month loaded on first load

--- site/src/js/app.js
+++ site/src/js/app.js
@@ -3,24 +3,24 @@ import "alpinejs";
 const shortMonthName = (monthNum = 0) => {
   return new Date(1, monthNum, 1).toLocaleString("default", {
     month: "short"
-  })
-}
+  });
+};
+
+const longMonthName = (monthNum = 0) => {
+  return new Date(1, monthNum, 1).toLocaleString("default", {
+    month: "long"
+  });
+};
 
 window.initApp = function(hugoFoods) {
   return {
     foods: hugoFoods,
     month: new Date().getMonth(),
-    prevMonth() {
-      this.month--;
-    },
-    nextMonth() {
-      this.month++;
-    },
-    countVisible(items) {
-      return items.reduce((carry, item) => { return carry + (item.best[shortMonthName(this.month)] ? 1 : 0)}, 0);
-    },
     shortMonthName(monthNum) {
       return shortMonthName(monthNum);
+    },
+    longMonthName(monthNum) {
+      return longMonthName(monthNum);
     }
   };
 }
